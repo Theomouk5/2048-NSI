4af475815c043df64280f501dab587c976e4c6a6
Theomouk5 <theomoukilou@gmail.com>
2026-03-05 18:20:30 +0100

    Avancement functions.py
    - Ajout de la fonction pour la touche du bas
    - TODO : Faire fonction pour la touche du haut

--- functions.py
+++ functions.py
@@ -16,3 +16,23 @@ def additionnerGauche(tab):
                     tab[i][j-1] = tab[i][j] * 2
                     tab[i][j] = 0
     return tab
+
+def additionnerBas(tab):
+    for colonne in range(len(tab)):
+        for ligne in range(len(tab)-1):
+            if tab[ligne][colonne] != 0:
+                if tab[ligne][colonne] == tab[ligne+1][colonne]:
+                    tab[ligne+1][colonne] = tab[ligne][colonne] * 2
+                    tab[ligne][colonne] = 0
+    return tab
+
+matrice = [
+    [2, 0, 0, 2],
+    [2, 2, 8, 2],
+    [0, 0, 8, 4],
+    [0, 0, 0, 8]
+]
+
+print(matrice)
+matrice = additionnerBas(matrice)
+print(matrice)
