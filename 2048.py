b6cb023aee322f3ef7907efb6c7d7c5b480e3236
fourshoure <capareren45@gmail.com>
2026-03-05 13:32:45 +0000

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,3 +1,27 @@
+import tkinter as tk
+
+#fenetre du jeu
+fenetre = tk.Tk()
+fenetre.title("2048")
+
+#la grille
+for ligne in range(4):
+    for colonne in range(4):
+        case = tk.Label(
+            fenetre,
+            text="",
+            width=6,
+            height=3,
+            font=("Arial", 24),
+            borderwidth=2,
+            relief="solid"
+        )
+        case.grid(row=ligne, column=colonne, padx=5, pady=5)
+
+fenetre.mainloop()
+
+
+
 def main():
     print("Hello World")
 
