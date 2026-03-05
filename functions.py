04b2f0615cf0ee0901bf6516b59e5ffa60aa13e3
Theomouk5 <theomoukilou@gmail.com>
2026-03-05 22:31:46 +0100

    Resolution bug fonction directionnelles
    - HOTFIX: ajout d'une verification en plus

--- functions.py
+++ functions.py
@@ -1,71 +1,85 @@
-def additionnerDroite(tab):
-    for i in range(len(tab)):
-        for j in range(len(tab[i])-1, -1, -1):
-            if tab[i][j] == 0:
-                for k in range(j-1, -1, -1):
-                    if tab[i][k] != 0:
-                        tab[i][j] = tab[i][k]
-                        tab[i][k] = 0
-                        break
+def _verificationDroite(tab, ligne):
+    for colonne in range(len(tab[ligne])-1, -1, -1):
+        if tab[ligne][colonne] == 0:
+            for k in range(colonne-1, -1, -1):
+                if tab[ligne][k] != 0:
+                    tab[ligne][colonne] = tab[ligne][k]
+                    tab[ligne][k] = 0
+                    break
 
-        for j in range(len(tab[i]) - 1):
-            if tab[i][j] != 0:
-                if tab[i][j] == tab[i][j+1]:
-                    tab[i][j+1] = tab[i][j] * 2
-                    tab[i][j] = 0
+def _verificationGauche(tab, ligne):
+    for colonne in range(len(tab[ligne])):
+        if tab[ligne][colonne] == 0:
+            for k in range(colonne, len(tab)):
+                if tab[ligne][k] != 0:
+                    tab[ligne][colonne] = tab[ligne][k]
+                    tab[ligne][k] = 0
+                    break
+
+def _verificationBas(tab, colonne):
+    for ligne in range(len(tab)-1, 0, -1):
+        if tab[ligne][colonne] == 0:
+            for k in range(ligne-1, -1, -1):
+                if tab[k][colonne] != 0:
+                    tab[ligne][colonne] = tab[k][colonne]
+                    tab[k][colonne] = 0
+                    break
+
+def _verificationHaut(tab, colonne):
+    for ligne in range(len(tab)):
+        if tab[ligne][colonne] == 0:
+            for k in range(ligne, len(tab)):
+                if tab[k][colonne] != 0:
+                    tab[ligne][colonne] = tab[k][colonne]
+                    tab[k][colonne] = 0
+                    break
+
+
+
+def additionnerDroite(tab):
+    for ligne in range(len(tab)):
+        _verificationDroite(tab, ligne) 
+        for colonne in range(len(tab[ligne]) - 1):
+            if tab[ligne][colonne] != 0:
+                if tab[ligne][colonne] == tab[ligne][colonne+1]:
+                    tab[ligne][colonne+1] = tab[ligne][colonne] * 2
+                    tab[ligne][colonne] = 0
+                    _verificationDroite(tab, ligne)
                     break
     return tab
 
 def additionnerGauche(tab):
-    for i in range(len(tab)):
-        for j in range(len(tab[i])):
-            if tab[i][j] == 0:
-                for k in range(j, len(tab)):
-                    if tab[i][k] != 0:
-                        tab[i][j] = tab[i][k]
-                        tab[i][k] = 0
-                        break
-
-        for j in range(len(tab[i])-1, -1, -1):
-            if tab[i][j] != 0:
-                if tab[i][j] == tab[i][j-1]:
-                    tab[i][j-1] = tab[i][j] * 2
-                    tab[i][j] = 0
+    for ligne in range(len(tab)):
+        _verificationGauche(tab, ligne)
+        for colonne in range(len(tab[ligne])-1, -1, -1):
+            if tab[ligne][colonne] != 0:
+                if tab[ligne][colonne] == tab[ligne][colonne-1]:
+                    tab[ligne][colonne-1] = tab[ligne][colonne] * 2
+                    tab[ligne][colonne] = 0
+                    _verificationGauche(tab, ligne)
                     break
     return tab
 
 def additionnerBas(tab):
     for colonne in range(len(tab)):
-        for ligne in range(len(tab)-1, 0, -1):
-            if tab[ligne][colonne] == 0:
-                for k in range(ligne-1, -1, -1):
-                    if tab[k][colonne] != 0:
-                        tab[ligne][colonne] = tab[k][colonne]
-                        tab[k][colonne] = 0
-                        break
-
+        _verificationBas(tab, colonne)
         for ligne in range(len(tab)-1):
             if tab[ligne][colonne] != 0:
                 if tab[ligne][colonne] == tab[ligne+1][colonne]:
                     tab[ligne+1][colonne] = tab[ligne][colonne] * 2
                     tab[ligne][colonne] = 0
+                    _verificationBas(tab, colonne)
                     break
     return tab
 
 def additionnerHaut(tab):
     for colonne in range(len(tab)):
-        for ligne in range(len(tab)):
-            if tab[ligne][colonne] == 0:
-                for k in range(ligne, len(tab)):
-                    if tab[k][colonne] != 0:
-                        tab[ligne][colonne] = tab[k][colonne]
-                        tab[k][colonne] = 0
-                        break
-
+        _verificationHaut(tab, colonne)
         for ligne in range(len(tab)-1, 0, -1):
             if tab[ligne][colonne] != 0:
                 if tab[ligne][colonne] == tab[ligne-1][colonne]:
                     tab[ligne-1][colonne] = tab[ligne][colonne] * 2
                     tab[ligne][colonne] = 0
+                    _verificationHaut(tab, colonne)
                     break
     return tab
